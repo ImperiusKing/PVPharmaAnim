5b1a25db734964f4410261eab2e79c30e12e734e
ImperiusKing <stapgold.fsc@gmail.com>
2023-08-17 16:58:51 +0700

    feat: add page transition

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,24 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import { Inter } from 'next/font/google';
+ 
+// font settings
+//const inter = Inter({ subsets: ['latin'] });
+
+// components
+import Nav from '../components/Nav';
+import Header from '../components/Header';
+import TopLeftImg from '../components/TopLeftImg';
+
+const Layout = ({children}) => {
+  return (
+    <div 
+      className={`page bg-site text-white bg-cover bg-no-repeat font-inter relative`}
+    >
+      <Nav />
+      <Header />
+      <TopLeftImg />
+      {children}
+  </div>
+  );
 };
 
 export default Layout;
--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -1,5 +1,20 @@
+import Image from "next/image";
+
+
 const TopLeftImg = () => {
-  return <div>Top Left Image</div>;
+  return (
+    <div className='absolute left-0 top-0 z-10 w-[200px] xl:w-[400px]'>
+      <Image src='/PhucVinh_Logo PNG-03.png' width={400} height={400} alt='' />
+    </div>
+  );
 };
+
+// const TopLeftImg = () => {
+//   return (
+//     <div className='absolute inset-0 z-10'>
+//       <Image src='/top-.png' fill alt='' />
+//     </div>
+//   );
+// };
 
 export default TopLeftImg;
