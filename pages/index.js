194588a8231e4f2c13665c2528709a42622d1fee
ImperiusKing <stapgold.fsc@gmail.com>
2023-08-18 13:35:07 +0700

    add cms: not working

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,58 @@
-const Home = () => {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useQuery } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+import { gql } from '@apollo/client';
+import client from '../pages/apollo-client'; // Relative path to apolloClient.js
+import App from '../pages/_app'; // Your main application component
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
+const Home = ({ page }) => {
+  const { loading, error, data } = useQuery(GET_PAGE_DATA);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  // Use the data retrieved from the query here
+
   return <div>Home</div>;
 };
 
-export default Home;
+export async function getStaticProps() {
+  // Use useQuery hook from @apollo/client to fetch the data in the component
+  const { loading, error, data } = useQuery(GET_PAGE_DATA);
+
+  if (loading) return { props: { page: [] } };
+  if (error) return { props: { page: [] } };
+
+  return {
+    props: {
+      page: data.page.slice(0, 11),
+    },
+  };
+}
+
+const GET_PAGE_DATA = gql`
+  query PageHome {
+    page(where: { slug: "home" }) {
+      heroLink
+      heroText
+      heroTitle
+      id
+      name
+      slug
+      heroBackground {
+        width
+        url
+        height
+      }
+    }
+  }
+`;
+
