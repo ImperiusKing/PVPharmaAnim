64857e647e1da77f6be67b32cb0e53a5fa554f14
ImperiusKing <stapgold.fsc@gmail.com>
2023-08-30 16:49:53 +0700

    add: achievement

--- components/Layout.js
+++ components/Layout.js
@@ -6,15 +6,16 @@ import { Inter } from 'next/font/google';
 // components
 // import Nav from '../components/Nav';
 import Header from '../components/Header';
+import Achievement from '../pages/achievement';
 
 const Layout = ({children}) => {
   return (
     <div 
       className={`page bg-site text-white bg-cover bg-no-repeat font-inter relative`}
     >
-      {/* <Nav /> */}
       <Header />
       {children}
+      <Achievement/>
   </div>
   );
 };
